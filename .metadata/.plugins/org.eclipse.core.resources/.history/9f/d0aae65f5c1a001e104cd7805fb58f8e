73ce511be9c94e5952c1540286aec81439c3101f
daru <daru1@gmail.com>
2023-07-05 10:58:09 +0530

    jdbc connection on going

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9f/d0aae65f5c1a001e104cd7805fb58f8e
@@ -0,0 +1,5 @@
+package com.register.online.login;
+
+public class registerdto {
+
+}
--- Registration/src/main/java/com/register/online/login/registerdto.java
+++ Registration/src/main/java/com/register/online/login/registerdto.java
@@ -1,5 +1,34 @@
 package com.register.online.login;
 
 public class registerdto {
+	private int regid;
+	private String username;
+	private String place;
+	private String mobile;
+	public int getRegid() {
+		return regid;
+	}
+	public void setRegid(int regid) {
+		this.regid = regid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
 
 }
